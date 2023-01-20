04de24bee23e28124fb6b38d928849f4eeeddce9
Jan Mielziuk <jan.jakub.mielziuk@gmail.com>
2023-01-20 15:43:18 +0100

    6.2 - Zadanie: pierwszy test jednostkowy

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,44 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
+import java.sql.SQLOutput;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania.");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+        Calculator calculator1 = new Calculator(2, 2);
+        Calculator calculator2 = new Calculator(4, 4);
+
+        int result1 = calculator1.add();
+        int result2 = calculator2.subtract();
+
+        if (result1 == calculator1.getA() + calculator1.getB()){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        if (result2 == calculator2.getA() - calculator2.getB()){
+            System.out.println("test OK");
+        }  else {
+            System.out.println("Error!");
+        }
+
+
+
     }
 
 }
