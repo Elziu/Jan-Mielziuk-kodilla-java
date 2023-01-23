c7c1e6b3e2bd3a1bf624f2b2d09094a71c8f40fb
Jan Mielziuk <jan.jakub.mielziuk@gmail.com>
2023-01-23 14:43:06 +0100

    6.3 Odds exterminator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -7,36 +7,6 @@ import java.sql.SQLOutput;
 
 public class TestingMain {
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        System.out.println("Test - pierwszy test jednostkowy:");
-
-        Calculator calculator1 = new Calculator(2, 2);
-        Calculator calculator2 = new Calculator(4, 4);
-
-        int result1 = calculator1.add();
-        int result2 = calculator2.subtract();
-
-        if (result1 == calculator1.getA() + calculator1.getB()){
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        if (result2 == calculator2.getA() - calculator2.getB()){
-            System.out.println("test OK");
-        }  else {
-            System.out.println("Error!");
-        }
-
 
 
     }
---
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -0,0 +1,50 @@
+package com.kodilla.testing.collection;
+import com.kodilla.testing.collection.OddNumbersExterminator;
+import java.util.*;
+import org.junit.jupiter.api.*;
+
+
+
+public class CollectionTestSuite {
+    @BeforeEach
+    public void before(){
+        System.out.println("Test case: begin");
+    }
+    @AfterEach
+    public void after(){
+        System.out.println("Test case: end");
+    }
+    @DisplayName(
+            "Does class work when list is empty."
+    )
+    @Test
+    public void testOddNumbersExterminatorEmptyList(){
+
+        //Given
+        ArrayList<Integer> emptyList = new ArrayList<Integer>();
+
+        //When
+        ArrayList<Integer> listNumbers = OddNumbersExterminator.exterminate(emptyList);
+        System.out.println("Testing" + listNumbers);
+
+        //Then
+        Assertions.assertEquals(emptyList,listNumbers);
+    }
+    @DisplayName(
+            "Does class work when list contains odds and evens."
+    )
+    @Test
+    public void testOddNumbersExterminatorNormalList(){
+
+        //Given
+        ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(364,555,15,400,23,74,23,3,20));
+        ArrayList<Integer> oddList = new ArrayList<Integer>(Arrays.asList(364,400,74,20));
+
+        //When
+        ArrayList<Integer> listNumbers = OddNumbersExterminator.exterminate(list);
+        System.out.println("Testing" + listNumbers);
+
+        //Then
+        Assertions.assertEquals(oddList,listNumbers);
+    }
+}
